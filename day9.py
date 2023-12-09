467e8bec253502397e0aec150f0bed6ef26b1f0f
itamar <itamarcu@gmail.com>
2023-12-09 12:08:46 +0200

    clean up day 9 to remove code repetition

--- day9.py
+++ day9.py
@@ -1,12 +1,13 @@
+from typing import List
+
+
 def main():
     with open("input.txt") as input_file:
         input_lines = input_file.readlines()
         input_lines = [line.strip('\n') for line in input_lines]
 
-    sum_of_nexts = 0
-    for line in input_lines:
-        level_0 = [int(n) for n in line.split(" ")]
-        levels = [level_0]
+    def extrapolate_next_value(sequence: List[int]) -> int:
+        levels = [sequence]
         while any(n != 0 for n in levels[-1]):
             levels.append([
                 levels[-1][i + 1] - levels[-1][i] for i in range(len(levels[-1]) - 1)
@@ -16,26 +17,14 @@ def main():
         for i in range(1, len(levels)):
             next_one = levels[-i][-1] + levels[-1 - i][-1]
             levels[-1 - i].append(next_one)
-        sum_of_nexts += levels[0][-1]
+        return levels[0][-1]
+
+    sum_of_nexts = sum(extrapolate_next_value([int(n) for n in line.split(" ")]) for line in input_lines)
     print(sum_of_nexts)  # 2008960228
 
-    # now do it backwards
-    # by reversing input lines one by one
-    sum_of_nexts = 0
-    for line in input_lines:
-        level_0 = [int(n) for n in reversed(line.split(" "))]
-        levels = [level_0]
-        while any(n != 0 for n in levels[-1]):
-            levels.append([
-                levels[-1][i + 1] - levels[-1][i] for i in range(len(levels[-1]) - 1)
-            ])
-        # iterate backwards on levels by index
-        levels[-1].append(0)
-        for i in range(1, len(levels)):
-            next_one = levels[-i][-1] + levels[-1 - i][-1]
-            levels[-1 - i].append(next_one)
-        sum_of_nexts += levels[0][-1]
-    print(sum_of_nexts)  # 1097
+    # now extrapolate backwards, by just extrapolating forwards on reversed sequences
+    sum_of_prevs = sum(extrapolate_next_value([int(n) for n in reversed(line.split(" "))]) for line in input_lines)
+    print(sum_of_prevs)  # 1097
 
 
 if __name__ == '__main__':
