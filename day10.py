99fba08e97b3972de558eda00045c739064dac0e
itamar <itamarcu@gmail.com>
2023-12-10 12:41:31 +0200

    day 10 part 2

--- day10.py
+++ day10.py
@@ -1,55 +1,98 @@
-from collections import deque
-
-
 def main():
     with open("input.txt") as input_file:
         input_lines = input_file.readlines()
         input_lines = [line.strip('\n') for line in input_lines]
-    dirs = {
-        # all in (r, c) notation
-        '|': ((-1, 0), (+1, 0)),
-        '-': ((0, +1), (0, -1)),
-        'L': ((-1, 0), (0, +1)),
-        'J': ((-1, 0), (0, -1)),
-        '7': ((+1, 0), (0, -1)),
-        'F': ((+1, 0), (0, +1)),
-        '.': (),
-    }
-    grid = [list(line) for line in input_lines]
-    height = len(grid)
-    width = len(grid[0])
-    start_r_c = (-1, -1)
-    for r in range(height):
-        for c in range(width):
-            if grid[r][c] == 'S':
-                start_r_c = (r, c)
+    # ADD PADDING - this is helpful for part 2's fill and to avoid boundary checks in part 1
+    input_lines = ["#" + line + "#" for line in input_lines]
+    input_lines = ["#" * len(input_lines[0])] + input_lines + ["#" * len(input_lines[0])]
+    up, down, left, right, = -1, +1, -1j, +1j
+    grid = {}
+    start_coords = -1 - 1j
+    for r, line in enumerate(input_lines):
+        for c, char in enumerate(line):
+            pos = r + c * 1j
+            grid[pos] = char
+            if char == 'S':
+                start_coords = pos
+    for start_direction in [up, down, left, right]:
+        position = start_coords
+        direction = start_direction
+        visited = set()
+        path_taken = []
+        while position not in visited:
+            visited.add(position)
+            position += direction
+            if grid[position] in 'S.#':
                 break
-    sr, sc = start_r_c
-    visited = set()
-    arms = deque()
-    if (+1, 0) in dirs[grid[sr - 1][sc]]:
-        arms.append((sr - 1, sc, sr, sc, 1))
-    if (-1, 0) in dirs[grid[sr + 1][sc]]:
-        arms.append((sr + 1, sc, sr, sc, 1))
-    if (0, +1) in dirs[grid[sr][sc - 1]]:
-        arms.append((sr, sc - 1, sr, sc, 1))
-    if (0, -1) in dirs[grid[sr][sc + 1]]:
-        arms.append((sr, sc + 1, sr, sc, 1))
-    while arms:
-        r, c, from_r, from_c, dist = arms.popleft()
-        if r < 0 or r >= height or c < 0 or c >= width:
-            continue
-        if grid[r][c] == '.' or grid[r][c] == 'S':
-            continue
-        if (r, c) in visited:
-            print(dist)  # 6754
+            prev_direction = direction
+            if grid[position] == '|' and direction in [up, down]:
+                pass  # still the same direction
+            elif grid[position] == '-' and direction in [left, right]:
+                pass  # still the same direction
+            elif any([
+                grid[position] == 'L' and direction == down,
+                grid[position] == 'J' and direction == right,
+                grid[position] == '7' and direction == up,
+                grid[position] == 'F' and direction == left,
+            ]):
+                direction = direction * +1j  # turn left
+            elif any([
+                grid[position] == 'L' and direction == left,
+                grid[position] == 'J' and direction == down,
+                grid[position] == '7' and direction == right,
+                grid[position] == 'F' and direction == up,
+            ]):
+                direction = direction * -1j  # turn right
+            else:
+                position = None
+                break
+            path_taken.append((position, direction, prev_direction))
+            # BIG ASSUMPTION:  loop goes counterclockwise (arbitrary)
+        if position == start_coords:  # successful loop
             break
-        visited.add((r, c))
-        assert grid[r][c] in dirs
-        for dr, dc in dirs[grid[r][c]]:
-            if (r + dr, c + dc) == (from_r, from_c):
+    print("Part 1:", len(visited) // 2)  # 6754
+
+    # now "fill" from those spots
+    for attempt in ["clockwise", "counterclockwise"]:
+        marked_inside_loop = set()
+        if attempt == "clockwise":
+            for position, direction, prev_direction in path_taken:
+                if direction == prev_direction:  # if kept moving forwards, mark our relative right
+                    marked_inside_loop.add(position + direction * -1j)
+                if direction == prev_direction * +1j:  # if turned left, mark our relative down and right
+                    marked_inside_loop.add(position + direction * -1)
+                    marked_inside_loop.add(position + direction * -1j)
+                if direction == prev_direction * -1j:  # if turned right, mark nothing
+                    pass
+        elif attempt == "counterclockwise":
+            for position, direction, prev_direction in path_taken:
+                if direction == prev_direction:  # if kept moving forwards, mark our relative left
+                    marked_inside_loop.add(position + direction * +1j)
+                if direction == prev_direction * -1j:  # if turned right, mark our relative down and left
+                    marked_inside_loop.add(position + direction * -1)
+                    marked_inside_loop.add(position + direction * +1j)
+                if direction == prev_direction * +1j:  # if turned left, mark nothing
+                    pass
+        marked_inside_loop.difference_update(visited)
+        # greedy fill, it's fine because we'll break if we reach outer bounds
+        filled = set()
+        frontier = list(marked_inside_loop)
+        while frontier:
+            position = frontier.pop()
+            if grid[position] == "#":
+                # artificial boundary, `break` will prevent `else: break` and thus continue for another attempt
+                break
+            if position in filled:
                 continue
-            arms.append((r + dr, c + dc, r, c, dist + 1))
+            if position in visited:
+                continue
+            filled.add(position)
+            for direction in [up, down, left, right]:
+                new_position = position + direction
+                frontier.append(new_position)
+        else:  # did not break, i.e. did not reach outer bounds
+            break
+    print("Part 2:", len(filled))  # 567
 
 
 if __name__ == '__main__':
