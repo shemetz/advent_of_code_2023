38840f468ff28283434c4e1b7d0fca331dc166a6
itamar <itamarcu@gmail.com>
2023-12-12 08:06:20 +0200

    clean up day 12

--- day12.py
+++ day12.py
@@ -6,31 +6,43 @@ with open("input.txt") as input_file:
     input_lines = [line.strip('\n') for line in input_lines]
 
 
-@lru_cache()
+@lru_cache
 def recursive_arrangements(pixels: str, groups: Tuple[int]):
-    if all(c == "." for c in pixels):
-        if len(groups) == 0:
-            return 1
-        else:
-            return 0
+    if len(pixels) == 0:
+        # e.g. "" (,)
+        # e.g. "" (1,)
+        return 1 if len(groups) == 0 else 0
     if pixels.startswith("."):
+        # e.g. ".###?" (4,)
+        # -> "###?" (4,)
         return recursive_arrangements(pixels.strip("."), groups)
+    if pixels.startswith("?"):
+        # e.g. "?###?" (4,)
+        # 1-> ".###?" (4,)
+        # 2-> "####?" (4,)
+        return recursive_arrangements(pixels.replace("?", ".", 1), groups) \
+            + recursive_arrangements(pixels.replace("?", "#", 1), groups)
     if pixels.startswith("#"):
         if len(groups) == 0:
+            # e.g. "##" (,)
             return 0
         if len(pixels) < groups[0]:
+            # e.g. "##" (3,)
             return 0
         if any(c == "." for c in pixels[0:groups[0]]):
+            # e.g. "##.???" (3,1)
             return 0
         if len(groups) > 1:
             if len(pixels) < groups[0] + 1 or pixels[groups[0]] == "#":
                 return 0
+            # e.g. "##.???" (2,1)
+            # -> "???" (1,)
             return recursive_arrangements(pixels[groups[0] + 1:], groups[1:])
         else:
+            # e.g. "##.???" (2,)
+            # -> ".???" (,)
             return recursive_arrangements(pixels[groups[0]:], groups[1:])
-    if pixels.startswith("?"):
-        return recursive_arrangements(pixels.replace("?", ".", 1), groups) \
-            + recursive_arrangements(pixels.replace("?", "#", 1), groups)
+    raise Exception("no other branches possible")
 
 
 for part in [1, 2]:
